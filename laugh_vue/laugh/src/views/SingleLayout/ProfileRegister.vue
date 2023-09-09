874caee065d0d2bcc5af3635625267f24be605e7
j-nishiki <j.nishiki@c4c.co.jp>
2023-09-10 01:05:07 +0900

    ステップバーの実装

--- laugh_vue/laugh/src/views/SingleLayout/ProfileRegister.vue
+++ laugh_vue/laugh/src/views/SingleLayout/ProfileRegister.vue
@@ -1,6 +1,13 @@
 <template>
+  {{ tokuibunya }}
   <v-container>
-    <v-row>ステップバーを書く(錦)</v-row>
+    <v-row justify="center" align-content="center">
+      <ul class="step-box">
+        <li class="step-1"><div class="step-num">１</div><div class="step-desc">会員登録</div></li>
+        <li class="step-2"><div class="step-num">２</div><div class="step-desc">本人情報の入力</div></li>
+        <li class="step-3"><div class="step-num">３</div><div class="step-desc">本人認証</div></li>
+      </ul>
+    </v-row>
     <v-row>
       <v-card
         class="mx-auto w-50"
@@ -13,27 +20,144 @@
 
         <v-card-text>
           <v-form @submit.prevent>
-            <v-text-field
+            <!-- <v-text-field
               v-model="firstName"
               :rules="rules"
               label="名前"
-            ></v-text-field>
+            ></v-text-field> -->
             <v-text-field
-              v-model="firstName"
+              v-model="email"
               :rules="rules"
               label="メールアドレス"
             ></v-text-field>
             <v-text-field
-              v-model="firstName"
-              :rules="rules"
-              label="ほげげ"
+              v-model="password1"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              label="パスワード"
+              counter
+              @click:append="show1 = !show1"
             ></v-text-field>
             <v-text-field
-              v-model="firstName"
-              :rules="rules"
-              label="ほげ"
+              v-model="password2"
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show2 ? 'text' : 'password'"
+              label="【確認用】パスワード"
+              counter
+              @click:append="show2 = !show2"
             ></v-text-field>
-            <v-btn type="submit" block class="mt-2">Submit</v-btn>
+            <v-radio-group v-model="row" inline>
+              <v-radio
+                label="作家"
+                value="sakka"
+                color="orange"
+              ></v-radio>
+              <v-radio
+                label="芸人"
+                value="geinin"
+                color="orange"
+              ></v-radio>
+            </v-radio-group>
+            <v-text-field
+              v-model="gropName"
+              :rules="rules"
+              label="活動名"
+            ></v-text-field>
+            <v-text-field
+              v-model="gropNameKana"
+              :rules="rules"
+              label="活動名（カナ）"
+            ></v-text-field>            
+            <v-radio-group v-model="sei" inline>
+              <v-radio
+                label="回答なし"
+                value="seiNull"
+                color="orange"
+              ></v-radio>
+              <v-radio
+                label="男性"
+                value="man"
+                color="orange"
+              ></v-radio>
+              <v-radio
+                label="女性"
+                value="man"
+                color="orange"
+              ></v-radio>
+              <v-radio
+                label="男女"
+                value="ManMoman"
+                color="orange"
+              ></v-radio>
+            </v-radio-group>
+            <v-select :items="geininsakka" label="所属事務所"></v-select>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="漫才"
+                color="orange"
+                value="manzai"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="ピン"
+                color="orange"
+                value="pin"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="コント"
+                color="orange"
+                value="konto"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="ギャグ"
+                color="orange"
+                value="gyagu"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="モノマネ"
+                color="orange"
+                value="monomane"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="歌ネタ"
+                color="orange"
+                value="utaneta"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="リズムネタ"
+                color="orange"
+                value="rizumuneta"
+                hide-details
+              ></v-checkbox>
+              <v-checkbox
+                v-model="tokuibunya"
+                label="その他"
+                color="orange"
+                value="sonota"
+                hide-details
+              ></v-checkbox>
+            <v-select :items="tihou" label="活動場所"></v-select>
+            <v-textarea 
+              v-model="selfIntroduction" 
+              outlined 
+              solo 
+              name="input-7-4" 
+              label="自己紹介" 
+            ></v-textarea>
+            <v-btn type="submit" block class="mt-2">送信</v-btn>
           </v-form>
         </v-card-text>
       </v-card>
@@ -52,307 +176,134 @@ const router = useRouter();
 const route = useRoute();
 // setupをscriptタグに書くことによってexport defaultせずに使えるようになる compositionAPIというvue3からの機能
 const test = ref('test');
-test.value = "aiueo";
-// //画像のアップロード
+// test.value = "aiueo";
+const test2 = ref(['aa']);
 
-// new Vue({
-//   el: '#app',
-//   vuetify: new Vuetify(),
-  
-// })
+const geininsakka = ref(['アヴィラ', '浅井企画', 'ASH&Dコーポレーション', 'アミー・パーク', 'UMEDA芸能', 'FMG', 'MLクリエーション', '大川興業', '太田プロダクション', 'オフィス北野', 'オリジン・コーポレーション'
+                   , 'お笑い集団ティーライズ', 'グレープカンパニー', 'ケイダッシュステージ', 'K-PRO', 'ザ・森東', 'サンミュージックプロダクション', 'SHUプロモーション', '松竹芸能', 'スパンキープロダクション'
+                   , 'ソニー・ミュージックアーティスツ', 'タイタン', 'トゥインクル・コーポレーション', '徳間ジャパンコミュニケーションズ', 'どっかんプロ', 'トップ・カラー', 'ナチュラルエイト', 'ニュースタッフプロダクション'
+                   , 'ノーリーズン', 'ファインステージ', 'プロダクション人力舎', 'プロデューサーハウスあ・うん', 'ホリプロ', 'ホリプロコム', 'マセキ芸能社', '三木プロダクション', '吉本興業', 'ワタナベエンターテインメント'
+                   , 'ワハハ本舗', '無所属']);
+const tihou = ref(['北海道', '東北', '関東', '北陸甲信越', '中部', '関西', '中国', '四国', '九州']);
 
-// export default {
-//   // data () {
-//   //     return {
-//   //       show1: false,
-//   //       password: '',
-//   //       rules: {
-//   //         required: value => !!value || '入力必須です。',
-//   //         min: v => v.length >= 8 || '8文字以上入力してください',
-//   //         emailMatch: () => (`The email and password you entered don't match`),
-//   //       },
-//   //     }
-//   //   }
+const show1 = ref(false);
+const show2 = ref(false);
+const password1 = ref('');
+const password2 = ref('');
+const row = ref(null);
+const sei = ref(null);
+const tokuibunya = ref([]);
 
-//     data: () => ({
+// passwordバリデーションチェック一旦保留
+const required1 = ref('');
+const min1 = ref('');
+const required2 = ref('');
+const min2 = ref('');
+const required = ref('');
+const min = ref('');
+const rules = ref(['aa','bb']);
+if ((password1.value).length == 0) {
+  required1.value = "パスワードを入力してください";
+}
+if ((password1.value).length < 8) {
+  min1.value = "パスワードは8文字以上、入力してください";
+}
+// -------------------------------
+
+// data: () => ({
 //       geininsakka: ['アヴィラ', '浅井企画', 'ASH&Dコーポレーション', 'アミー・パーク', 'UMEDA芸能', 'FMG', 'MLクリエーション', '大川興業', '太田プロダクション', 'オフィス北野', 'オリジン・コーポレーション'
 //                   , 'お笑い集団ティーライズ', 'グレープカンパニー', 'ケイダッシュステージ', 'K-PRO', 'ザ・森東', 'サンミュージックプロダクション', 'SHUプロモーション', '松竹芸能', 'スパンキープロダクション'
 //                   , 'ソニー・ミュージックアーティスツ', 'タイタン', 'トゥインクル・コーポレーション', '徳間ジャパンコミュニケーションズ', 'どっかんプロ', 'トップ・カラー', 'ナチュラルエイト', 'ニュースタッフプロダクション'
 //                   , 'ノーリーズン', 'ファインステージ', 'プロダクション人力舎', 'プロデューサーハウスあ・うん', 'ホリプロ', 'ホリプロコム', 'マセキ芸能社', '三木プロダクション', '吉本興業', 'ワタナベエンターテインメント'
 //                   , 'ワハハ本舗', '無所属'],
-//       tihou: ['北海道', '東北', '関東', '北陸甲信越', '中部', '関西', '中国', '四国', '九州'],
+//                   tihou: ['北海道', '東北', '関東', '北陸甲信越', '中部', '関西', '中国', '四国', '九州'],
+//       show1: false,
+//       show2: false,
+//       password1: '',
+//       password2: '',
+//       rules: {
+//           required: value => !!value || 'パスワードを入力してください',
+//           min: v => v.length >= 8 || 'パスワードは8文字以上、入力してください',
+//           matched: 
+//         },
 //     }),
 
-//     // data: () => ({
-//     //   geininsakka: ['アヴィラ', '浅井企画', 'ASH&Dコーポレーション', 'アミー・パーク', 'UMEDA芸能', 'FMG', 'MLクリエーション', '大川興業', '太田プロダクション', 'オフィス北野', 'オリジン・コーポレーション'
-//     //               , 'お笑い集団ティーライズ', 'グレープカンパニー', 'ケイダッシュステージ', 'K-PRO', 'ザ・森東', 'サンミュージックプロダクション', 'SHUプロモーション', '松竹芸能', 'スパンキープロダクション'
-//     //               , 'ソニー・ミュージックアーティスツ', 'タイタン', 'トゥインクル・コーポレーション', '徳間ジャパンコミュニケーションズ', 'どっかんプロ', 'トップ・カラー', 'ナチュラルエイト', 'ニュースタッフプロダクション'
-//     //               , 'ノーリーズン', 'ファインステージ', 'プロダクション人力舎', 'プロデューサーハウスあ・うん', 'ホリプロ', 'ホリプロコム', 'マセキ芸能社', '三木プロダクション', '吉本興業', 'ワタナベエンターテインメント'
-//     //               , 'ワハハ本舗', '無所属'],
-//     //   tihou: ['北海道', '東北', '関東', '北陸甲信越', '中部', '関西', '中国', '四国', '九州'],
-//     // }),
 
-//   //   data () {
-//   //   return {
-//   //     show1: false,
-//   //     show2: true,
-//   //     show3: false,
-//   //     show4: false,
-//   //     password: 'Password',
-//   //     rules: {
-//   //       required: value => !!value || 'Required.',
-//   //       min: v => v.length >= 8 || 'Min 8 characters',
-//   //       emailMatch: () => (`The email and password you entered don't match`),
-//   //     },
-//   //   }
-//   // },
-// }
-
+// //画像のアップロード
 </script>
 
 <style scoped>
 
-header {
-	background-color: antiquewhite;
+p {
+  color: red;
+  font-size: 12px;
 }
-
-.menu_top {
-	font-size: 56px;
-	color: #fd7e00;
-	font-weight: 700;
-}
-
-main {
-	width: 1200px;
-	background-color: white;
-	margin: 50px auto;
-}
-
-.user-register-box {
-	margin-top: -37px;
-	padding: 12px 12px 64px 12px;
-}
-
-.title-sub {
-	font-size: 32px;
-	font-weight: 800;
-	margin-top: 1px;
-	margin-left: 12px;
-	margin-bottom: 0px;
-}
-
-.user-register-list {
-	border-collapse:collapse;
-	margin-top: 12px;
-	margin-left: 12px;
-}
-
-.user-register-list th {
-	background-color: antiquewhite;
-	font-size: 20px;
-	text-align: center;
-	color: #fd7e00;
-	padding: 12px 16px;
-  
-}
-
-.user-register-list td {
-	font-size: 20px;
-	padding: 12px 16px;
-}
-
-.submit-button {
-	text-align: center;
-	margin-top: 12px;
-	margin-left: 12px;
-}
-
-.text-box {
-	padding: 10px 15px;
-	font-size: 16px;
-	border-radius: 3px;
-	border: 2px solid #ff9900;
-	box-sizing: border-box;
-}
-
-.text-box:focus {
-	border: 2px solid #ffcd90; 
-	z-index: 10;
-	outline: 0;
-}
-
-.psw-box {
-	padding: 10px 15px;
-	font-size: 16px;
-	border-radius: 3px;
-	border: 2px solid #ff9900;
-	box-sizing: border-box;
-}
-
-.psw-box:focus {
-	border: 2px solid #ffcd90;
-	background-color: #ffe6b4;
-	z-index: 10;
-	outline: 0;
-}
-
-.num-box {
-	padding: 10px 15px;
-	font-size: 16px;
-	border-radius: 3px;
-	border: 2px solid #ff9900;
-	box-sizing: border-box;
-}
-
-.num-box:focus {
-	border: 2px solid #ffcd90;
-	z-index: 10;
-	outline: 0;
-}
-
-.slc-box {
-	position: relative;
-}
-
-.slc-box::before,
-.slc-box::after {
-	position: absolute;
-	content: '';
-	pointer-events: none;
-}
-
-.slc-box::before {
-	right: 0;
-	display: inline-block;
-	width: 42px;
-	height: 42px;
-	border-radius: 0 3px 3px 0;
-	background-color: #ff9900;
-	content: '';
-}
-
-.slc-box::after {
-	position: absolute;
-	top: 50%;
-	right: 21px;
-	transform: translate(50%, -50%) rotate(45deg);
-	width: 6px;
-	height: 6px;
-	border-bottom: 3px solid #fff;
-	border-right: 3px solid #fff;
-	content: '';
-}
-
-.slc-box select {
-	appearance: none;
-	-webkit-appearance: none;
-	-moz-appearance: none;
-	min-width: 230px;
-	height: 2.8em;
-	padding: 10px 15px;
-	border: 2px solid #ff9900;
-	border-radius: 3px;
-	color: #333333;
-	font-size: 15px;
-	cursor: pointer;
-}
-
-.slc-box select:focus {
-	outline: 1px solid #ffcd90;
-}
-
-.self-introduction-box {
-	padding: 10px 15px;
-	font-size: 16px;
-	border-radius: 3px;
-	border: 2px solid #ff9900;
-	box-sizing: border-box;
-  width: 500px;
-  height: 200px;
-}
-
-.self-introduction-box:focus {
-	border: 2px solid #ffcd90;
-	background-color: #ffe6b4;
-	z-index: 10;
-	outline: 0;
-}
-
-.btn-submit-box {
-	text-align: center;
-	margin-top: 24px;
-}
-
-.btn-submit {
-	background-color: #ff9900;
-	font-size: 24px;
-	border-radius: 5px;
-	border: solid 3px #ff8800;
-	box-shadow: 3px 3px 3px gray;
-	transition: 0.3s;
-	padding: 4px 36px;
-	letter-spacing: 12px;
-}
-
-.btn-submit:hover {
-	background-color: #ffbb3c;
-}
-
-input {
-  margin-left: 12px;
-}
-
-.user-photo {
-  cursor: pointer;
-  outline: none;
-}
-
-.user-photo.default {
-  align-items: center;
-  background-color: #0074fb;
-  border: 1px solid #0051b0;
-  border-radius: 2px;
-  box-sizing: border-box;
-  display: inline-flex;
-  font-weight: 600;
-  justify-content: center;
-  letter-spacing: 0.3px;
-  color: #fff;
-  height: 4rem;
-  padding: 0 1.6rem;
-  max-width: 177px;
-}
-
-.user-photo.default:hover {
-  background-color: #4c9dfc;
-}
-
-.user-photo.default:active {
-  background-color: #0051b0;
-}
-
-.user-photo-image {
-  max-width: 85px;
-  display: block;
-}
-
-.user-photo-image:hover {
-  opacity: 0.8;
-}
-
-.file-button {
-  display: none;
-}
-
-.uploaded {
-  align-items: center;
+.step-box {
   display: flex;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 60px;
 }
 
-.d-block {
-  margin: 0 auto;
+.step-box > li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  text-align: center;
+}
+
+.step-box > li > .step-num  {
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  color: #f6f6f6;
+  justify-content: center;
+  text-align: center;
+  border-radius: 40px;
+  background-color: #cccccc;
+}
+
+.step-box > li > .step-desc {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #555555;
+  font-size: 24px;
+  bottom: -50px;
   width: 200px;
-  margin-top: 48px;
+  height: 37px;
+  text-align: center;
+  white-space: normal;
+  line-height: 15px;
+}
+
+.step-box > li + li {
+  position: relative;
+  margin-left: 260px;
+}
+
+.step-box > li + li:before {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: -256px;
+  width: 252px;
+  height: 1px;
+  background-color: #555555;
+  transform: translateY(-50%);
+}
+
+.step-box > li.done > .step-num,
+.step-box > li + li.done:before {
+  background-color: orange;
+}
+
+.step-box > li.done > .step-desc {
+  color: orange;
+  font-weight: bold;
 }
 
 </style>
