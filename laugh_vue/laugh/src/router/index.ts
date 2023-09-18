c485c7aff2b4b97cc44ae0bf8d884c553eadde6d
wataru1212 <w.suzuki.c4c2@gmail.com>
2023-09-18 15:00:27 +0900

    index.tsの誤字修正

--- laugh_vue/laugh/src/router/index.ts
+++ laugh_vue/laugh/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DemoView from '../views/DemoView.vue'
 import MyPage from '../views/MyPage.vue'
-import LoginViuw from '../views/SingleLayout/Login.vue'
+import LoginView from '../views/SingleLayout/Login.vue'
 import Main from '../views/Main.vue'
 
 const router = createRouter({
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: LoginViuw
+      component: LoginView
     },
     {
       path: '/', redirect: '/demo',component: Main,
