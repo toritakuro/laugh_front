2f14188d2c902cecdc320d95c0e8957df3626466
j-nishiki <j.nishiki@c4c.co.jp>
2023-10-08 20:57:16 +0900

    画像のアップロード　トリミング機能　実装途中

--- laugh_vue/laugh/src/views/SingleLayout/ProfileRegister.vue
+++ laugh_vue/laugh/src/views/SingleLayout/ProfileRegister.vue
@@ -234,7 +234,7 @@
             ></v-textarea>
 
             <!-- <img v-if="uploadImageUrl" :src="uploadImageUrl" /> -->
-            <ImgFileComponent2 @set-file="setFile"/>
+            <!-- <ImgFileComponent2 @set-file="setFile"/> -->
 
             <!-- <v-file-input
               v-model="input_image"
@@ -244,6 +244,24 @@
               @change="onImagePicked"
             ></v-file-input> -->
             <!-- <div>{{ input_image }}</div> -->
+            <!-- 画像トリミング新規登録はここを見る -->
+            <input ref="file" @change="setImage" type="file" name="image" accept="image/*" style="display: none;">
+            <div v-if="cropImg === ''" class="default profilePhoto" @click.prevent="showFileChooser">
+              <v-img
+                :aspect-ratio="1"
+                :src="src"
+              ></v-img>
+            </div>
+            <div v-if="cropImg !== ''" class="profilePhoto" @click.prevent="showFileChooser">
+              <v-img
+                :aspect-ratio="1"
+                :src="cropImg"
+                cover
+                class="rounded-lg"
+              ></v-img>
+            </div>
+            <ImageModalComponent :modelValue="modalFlg" :imgBase64="imgSrc" @update:modelValue="setModelValue" @update:imgValue="setImg"></ImageModalComponent>
+            <!-- 画像トリミング -->
             <v-col class="d-flex justify-center">
               <v-col cols="5">
                 <v-btn 
@@ -300,6 +318,64 @@ import { offices } from "../../types/Office";
 import { areas } from "../../types/Area";
 import { years } from "../../types/Year";
 import { months } from "../../types/Month";
+
+// 必要だ
+const modalFlg = ref(false)
+// 必要だ
+
+// 20230928 松本画像作業
+/** base64に変換 */
+const imgSrc = ref('');
+const cropImg = ref('');
+const file = ref();
+const src = ref("/img/man.svg");
+const setImage = (e: any) => {
+  const file = e.target.files[0];
+  if (!file.type.includes('image/')) {
+    alert('画像ファイルを選んでください');
+    return;
+  }
+  if (typeof FileReader === 'function') {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target != null) {
+        imgSrc.value = event.target.result as string;
+        // 画像の変更にはreplaceが必要らしい
+        //cropper.value.replace(event.target.result);
+      }
+    };
+    reader.readAsDataURL(file);
+  } else {
+    alert('FileReader APIがサポートされていません');
+  }
+  // 時差が必要みたい
+  setTimeout(() => {
+    console.log(11);
+    modalFlg.value = true;
+  }, 200);
+};
+/** emitで受けるメソッド */
+const setModelValue = (value:Boolean) => {
+  modalFlg.value = false;
+  file.value.value ='' // 初期化
+} 
+const setImg = (img:string) => {
+  cropImg.value = img;
+} 
+const showFileChooser = () => {
+  file.value.click();
+};
+// 必要だ
+// 20230928 松本画像作業
+const getData = async () => {
+  try {
+    const {data } = await http.get('/demo/list');
+    de = data.data;
+    console.log(de.id);
+  } catch (error) {
+
+  }
+}
 
 const router = useRouter();
 const route = useRoute();
