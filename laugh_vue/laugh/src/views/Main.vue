5df41e75b458d6d801a3b56ac32397e2b0f50358
鳥澤 拓朗 <toritakuro@me.com>
2023-10-17 01:21:00 +0900

    ログアウト処理

--- laugh_vue/laugh/src/views/Main.vue
+++ laugh_vue/laugh/src/views/Main.vue
@@ -75,10 +75,24 @@
         ヘルプ
       </v-btn>
 
-      <v-btn text @click="logout">
+      <v-btn text @click="openLogoutDialog">
         <v-icon>mdi-logout</v-icon>
         ログアウト
       </v-btn>
+      <v-dialog
+        v-model="dialog"
+        width="auto"
+      >
+      <v-card>
+        <v-card-text style="background-color: #f57c00; color: white;">
+          ログアウトします。よろしいですか？
+        </v-card-text>
+        <v-card-actions class="justify-center d-flex">
+          <v-btn color="'#F5F5F5'" @click="dialog = false">閉じる</v-btn>
+          <v-btn color="orange" @click=logout>はい</v-btn>
+        </v-card-actions>
+      </v-card>
+      </v-dialog>
     </v-app-bar>
 
     <v-main>
@@ -152,6 +166,7 @@
 
 <script>
 import { ref } from 'vue'
+import store from "@/store"
   import Message from '../components/MessageComponent.vue'
   const cards = ['Today', 'Yesterday']
   const links = [
@@ -181,10 +196,9 @@ export default {
       { title: '投稿一覧', link: '/posts-list' },
       { title: '退会(アカウント削除)', link: '/quit' },
     ],
+    dialog: false,
   }),
-}
-
-  // methods: {
+  methods: {
   //   goToMyPage() {
   //     // マイページへの遷移処理
   //     this.$router.push({ name: 'MyPage' });
@@ -201,9 +215,14 @@ export default {
   //     // ヘルプページへ遷移
   //     this.$router.push({ name: 'Help' });
   //   },
-  //   logout() {
-  //     // ログアウト処理
-  //     // ここにログアウト処理を記述
-  //   }
-  // }
+    openLogoutDialog() {
+      this.dialog = true;
+    },
+    logout() {
+      store.commit('token/removeToken');
+      this.$router.push({ name: 'login' });
+    }
+  }
+}
+
 </script>
