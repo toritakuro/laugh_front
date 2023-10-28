7fc1d6ba6a04b9e60007c5fbfbe1b1f3a8788703
hiro-mtmt <90678935+hiro-mtmt@users.noreply.github.com>
2023-10-28 11:12:26 +0900

    ユーザ詳細画面のLaughボタン実装

---
+++ laugh_vue/laugh/src/components/LaughApprovalModal.vue
@@ -0,0 +1,102 @@
+<template>
+    <div id="modal">
+      <div id="modal-content" class="modal">
+        <button @click="close()">X</button>
+        <p id="modal-message" class="modal__message">{{ message }}</p>
+        <span id="modal-cansel" class="modal__cancel" @click="refuse()">
+          いいえ
+        </span>
+        <button class="modal__btn" @click="approval()">はい</button>
+      </div>
+      <div id="modal-overlay"></div>
+    </div>
+  </template>
+  <script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps(["message"]);
+  const emit = defineEmits(['post-match']);
+
+  const refuse = () => {
+    emit("post-match", 99);
+  }
+
+  const approval = () => {
+    emit("post-match", 10);
+  }
+
+  const close = () => {
+    emit("post-match", );
+  }
+
+  </script>
+  <style scoped>
+  .modal {
+    padding: 10px 20px;
+    border: 2px solid #FB8C00;
+    background: #FFFF;
+    z-index: 2;
+    display: block;
+    text-align: center;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+  }
+  
+  .modal__message {
+    margin-top: 10px;
+  }
+  
+  .modal__cancel {
+    margin-right: 30px;
+    font-size: 15px;
+  }
+  
+  .modal__cancel:hover {
+    cursor: pointer;
+    color: #FB8C00;
+    font-weight: bold;
+  }
+  
+  .modal__btn {
+    display: inline-block;
+    margin: 30px auto;
+    text-decoration: none;
+    width: 100px;
+    height: 30px;
+    text-decoration: none;
+    color: #000000;
+    border: solid 2px #FB8C00;
+    border-radius: 3px;
+    transition: 0.4s;
+    text-align: center;
+    vertical-align: middle;
+    font-size: 15px;
+    background-color: #faebd7;
+  }
+  
+  .modal__btn:hover {
+    background: #FB8C00;
+    color: white;
+    cursor: pointer;
+  }
+  
+  #modal-overlay {
+    z-index: 1;
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 120%;
+    background-color: rgba(140, 140, 140, 0.75);
+  }
+  </style>
+  
+  
+  
+  
+  
+  
--- laugh_vue/laugh/src/views/UserDetail.vue
+++ laugh_vue/laugh/src/views/UserDetail.vue
@@ -1,30 +1,53 @@
 <template>
   <v-container fluid="true">
     <v-row>
-      <!-- <v-col cols="6" class="profile">
-        <v-container fluid="true">
-          <v-row>
-            <v-card class="mx-auto pa-2">
-              <v-row class="profile">
-                  <v-col cols="5" class="profile_left">
+      <v-col cols="6" >
+        <v-col>
+          <v-card class="mx-auto pa-2 detail_card">
+            <v-container fluid="true">
+              <v-row>
+                <v-col cols="5">
                   <v-img
                     :aspect-ratio="1"
                     src="https://comedian-new.com/wp-content/uploads/2020/01/samezombie.png"
                     cover
                     class="rounded-lg  profile_img"
                 ></v-img>
-                <v-card-title class="font-weight-black pt-0 pb-0 profile_info_name">{{ user?.userName }}</v-card-title>
                 </v-col>
-                <v-col cols="7" class="profile_right">
-                  <v-card-subtitle v-if="user?.gender == 1"><v-icon icon="mdi-gender-male-female" /> 男 </v-card-subtitle>
-                  <v-card-subtitle v-if="user?.gender == 2"><v-icon icon="mdi-gender-male-female" /> 女 </v-card-subtitle>
-                  <v-card-subtitle v-if="user?.gender == 3"><v-icon icon="mdi-gender-male-female" /> 男女 </v-card-subtitle>
-                  <v-card-text v-if="user?.userType == 1"><v-icon icon="mdi-account" /> {{ user?.memberNum }} 人</v-card-text>
-                  <v-card-text><v-icon icon="mdi-calendar-account-outline" /> {{ user?.activityDt }}</v-card-text>
-                  <v-card-subtitle><v-icon icon="mdi-office-building" /> {{ user?.officeName }}</v-card-subtitle>
-                  <v-card-subtitle><v-icon icon="mdi-map-marker" /> {{ user?.areaName }}</v-card-subtitle>
-                  <v-card-subtitle v-if="user?.userType == 1" class="mb-1 text-subtitle-1">芸風</v-card-subtitle>
-                  <v-card-subtitle v-if="user?.userType == 2" class="mb-1 text-subtitle-1">得意分野</v-card-subtitle>
+                <v-col class="pa-0 profile_info" lg="6" md="6" sm="12">
+                  <v-card-title class="font-weight-black pt-0 pb-0 profile_info_name">{{ user?.userName }}</v-card-title>
+                  <v-card-subtitle v-if="user?.gender == 1"><v-icon icon="mdi-gender-male-female" /> 性別： 男 </v-card-subtitle>
+                  <v-card-subtitle v-if="user?.gender == 2"><v-icon icon="mdi-gender-male-female" /> 性別： 女 </v-card-subtitle>
+                  <v-card-subtitle v-if="user?.gender == 3"><v-icon icon="mdi-gender-male-female" /> 性別： 男女 </v-card-subtitle>
+                  <v-card-subtitle v-if="user?.userType == 1"><v-icon icon="mdi-account" /> メンバー： {{ user?.memberNum }} 人</v-card-subtitle>
+                  <v-card-subtitle><v-icon icon="mdi-calendar-account-outline" /> 活動歴： {{ user?.activityDt }}</v-card-subtitle>
+                  <v-card-subtitle><v-icon icon="mdi-office-building" /> 事務所： {{ user?.officeName }}</v-card-subtitle>
+                  <v-card-subtitle><v-icon icon="mdi-map-marker" /> 活動地域： {{ user?.areaName }}</v-card-subtitle>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="profile_downside" v-if="user?.userType === 2">
+                  <v-card-subtitle class="mb-1 text-subtitle-1">得意分野</v-card-subtitle>
+                  <v-chip
+                    v-for="(itemName, i) in user?.comedyStyleNameList" :key="i"
+                    class="mr-1 mt-1 profile_chip"
+                    color="orange"
+                    text-color="white"
+                    >{{ itemName }}
+                  </v-chip>
+                  <v-card-subtitle class="mb-1 text-subtitle-1 profile_specialSkill_text">特殊スキル</v-card-subtitle>
+                  <v-chip
+                    v-for="(itemName, i) in user?.specialSkillNameList" :key="i"
+                    class="mr-1 mt-1"
+                    color="orange"
+                    text-color="white"
+                    >{{ itemName }}
+                  </v-chip>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="profile_downside" v-if="user?.userType === 1">
+                  <v-card-subtitle class="mb-1 text-subtitle-1">芸風</v-card-subtitle>
                   <v-chip
                     v-for="(itemName, i) in user?.comedyStyleNameList" :key="i"
                     class="mr-1 mt-1"
@@ -32,136 +55,72 @@
                     text-color="white"
                     >{{ itemName }}
                   </v-chip>
-                  <div v-if="user?.userType == 2">
-                    <v-card-subtitle class="mb-1 text-subtitle-1 profile_specialSkill_text">特殊スキル</v-card-subtitle>
-                      <v-chip
-                        v-for="(itemName, i) in user?.specialSkillNameList" :key="i"
-                        class="mr-1 mt-1"
-                        color="orange"
-                        text-color="white"
-                        >{{ itemName }}
-                      </v-chip>
-                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="profile_downside">
                   <v-card-subtitle class="mb-1 text-subtitle-1"><v-icon icon="mdi-comment-account-outline" />自己紹介</v-card-subtitle>
                   <v-card-text class="text-subtitle-1">{{ user?.selfIntroduction }}</v-card-text>
                 </v-col>
-                <v-col class="profile_downside" v-if="user?.userType === 1">
-                      
-                      
-                    </v-col>
               </v-row>
-              <v-row>
-                <v-btn>Laugh</v-btn>
-                <v-btn>superLaugh</v-btn>
-              </v-row>
-            </v-card>
-          </v-row>
-
-        </v-container>
-      </v-col> -->
-
-
-      
-      <v-col cols="6" >
-            <v-col>
-              <v-card class="mx-auto pa-2 detail_card">
-                <v-container fluid="true">
-                  <v-row>
-                    <v-col cols="5">
-                      <v-img
-                        :aspect-ratio="1"
-                        src="https://comedian-new.com/wp-content/uploads/2020/01/samezombie.png"
-                        cover
-                        class="rounded-lg  profile_img"
-                    ></v-img>
-                    </v-col>
-                    <v-col class="pa-0 profile_info" lg="6" md="6" sm="12">
-                      <v-card-title class="font-weight-black pt-0 pb-0 profile_info_name">{{ user?.userName }}</v-card-title>
-                      <v-card-subtitle v-if="user?.gender == 1"><v-icon icon="mdi-gender-male-female" /> 性別： 男 </v-card-subtitle>
-                      <v-card-subtitle v-if="user?.gender == 2"><v-icon icon="mdi-gender-male-female" /> 性別： 女 </v-card-subtitle>
-                      <v-card-subtitle v-if="user?.gender == 3"><v-icon icon="mdi-gender-male-female" /> 性別： 男女 </v-card-subtitle>
-                      <v-card-subtitle v-if="user?.userType == 1"><v-icon icon="mdi-account" /> メンバー： {{ user?.memberNum }} 人</v-card-subtitle>
-                      <v-card-subtitle><v-icon icon="mdi-calendar-account-outline" /> 活動歴： {{ user?.activityDt }}</v-card-subtitle>
-                      <v-card-subtitle><v-icon icon="mdi-office-building" /> 事務所： {{ user?.officeName }}</v-card-subtitle>
-                      <v-card-subtitle><v-icon icon="mdi-map-marker" /> 活動地域： {{ user?.areaName }}</v-card-subtitle>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col class="profile_downside" v-if="user?.userType === 2">
-                      <v-card-subtitle class="mb-1 text-subtitle-1">得意分野</v-card-subtitle>
-                      <v-chip
-                        v-for="(itemName, i) in user?.comedyStyleNameList" :key="i"
-                        class="mr-1 mt-1 profile_chip"
-                        color="orange"
-                        text-color="white"
-                        >{{ itemName }}
-                      </v-chip>
-                      <v-card-subtitle class="mb-1 text-subtitle-1 profile_specialSkill_text">特殊スキル</v-card-subtitle>
-                      <v-chip
-                        v-for="(itemName, i) in user?.specialSkillNameList" :key="i"
-                        class="mr-1 mt-1"
-                        color="orange"
-                        text-color="white"
-                        >{{ itemName }}
-                      </v-chip>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col class="profile_downside" v-if="user?.userType === 1">
-                      <v-card-subtitle class="mb-1 text-subtitle-1">芸風</v-card-subtitle>
-                      <v-chip
-                        v-for="(itemName, i) in user?.comedyStyleNameList" :key="i"
-                        class="mr-1 mt-1"
-                        color="orange"
-                        text-color="white"
-                        >{{ itemName }}
-                      </v-chip>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col class="profile_downside">
-                      <v-card-subtitle class="mb-1 text-subtitle-1"><v-icon icon="mdi-comment-account-outline" />自己紹介</v-card-subtitle>
-                      <v-card-text class="text-subtitle-1">{{ user?.selfIntroduction }}</v-card-text>
-                    </v-col>
-                  </v-row>
-
-
-                </v-container>
-              </v-card>
-            </v-col>
-          </v-col>
-
-
-
-          
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-col>   
       <v-col cols="6">
-      <v-card>
-      <v-tabs
-        v-model="tab"
-        color="orange-darken-2"
-      >
-        <v-tab value="profile" @click="getOogiri">
-          <v-icon middle size="x-large" icon="mdi-account" />
-          投稿一覧
-        </v-tab>
-        <v-tab value="oogiri" @click="getOogiri">
-          <v-icon middle size="x-large" icon="mdi-handshake" />
-          大喜利の投稿履歴
-        </v-tab>
-      </v-tabs>
-      <v-card-text>
-      <v-window v-model="tab">
-        <v-window-item value="profile">
-          <UserDetailOogiri ref="profileRef"></UserDetailOogiri>
-        </v-window-item>
-        <v-window-item value="oogiri">
-          <UserDetailOogiri ref="oogiriRef"></UserDetailOogiri>
-        </v-window-item>
-      </v-window>
-    </v-card-text>
-    </v-card>
+        <v-card>
+          <v-tabs
+            v-model="tab"
+            color="orange-darken-2"
+          >
+            <v-tab value="profile" @click="getOogiri">
+              <v-icon middle size="x-large" icon="mdi-account" />
+                投稿一覧
+            </v-tab>
+            <v-tab value="oogiri" @click="getOogiri">
+              <v-icon middle size="x-large" icon="mdi-handshake" />
+                大喜利の投稿履歴
+            </v-tab>
+          </v-tabs>
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item value="profile">
+                <UserDetailOogiri ref="profileRef"></UserDetailOogiri>
+              </v-window-item>
+              <v-window-item value="oogiri">
+                <UserDetailOogiri ref="oogiriRef"></UserDetailOogiri>
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
+    <div class="float-btn">
+      <div v-if="user?.matchStatus === undefined">
+        <v-btn color="orange-darken-1" size="default" @click="showModal()">match!!</v-btn>
+        <LaughModal
+        :message="sendMsg"
+        v-show="modal"
+        @post-match="postMatch"
+        ></LaughModal>
+      </div>
+      <div v-if="user?.matchStatus === 0">
+        <v-btn color="orange-darken-1" size="default" @click="showModal()">承認!!</v-btn>
+        <LaughApprovalModal
+        :message="recieveMsg"
+        v-show="modal"
+        @post-match="postMatch"
+        ></LaughApprovalModal>
+      </div>
+      <div v-if="user?.matchStatus === 10 || user?.matchStatus === 11">
+        <v-btn color="orange-darken-1" size="default" @click="showModal()">match解消</v-btn>
+        <LaughCancelModal
+        :message="cancelMsg"
+        v-show="modal"
+        @post-match="postMatch"
+        ></LaughCancelModal>
+      </div>
+    </div>
   </v-container>
   
   </template>
@@ -198,13 +157,23 @@
     margin-bottom: 20px;
   }
 
+  .float-btn {
+  position: absolute;
+  top: 250px;
+  right: 20px;
+  z-index: 100;
+}
+
 </style>
 
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue'
+import { computed, ref, onMounted, defineProps } from 'vue'
 import { useRoute } from 'vue-router'
 import MyPageProfile from '@/components/MyPageProfile.vue';
 import UserDetailOogiri from '@/components/UserDetailOogiri.vue'
+import LaughModal from "@/components/LaughModal.vue";
+import LaughApprovalModal from "@/components/LaughApprovalModal.vue";
+import LaughCancelModal from "@/components/LaughCancelModal.vue";
 import http from "@/http-common"
 import type User from "@/types/User";
 
@@ -243,4 +212,27 @@ const route = useRoute()
     console.log("user",user.value)
     getOogiri();
   }
+
+const sendMsg = "どちらを送りますか？";
+const recieveMsg = "マッチングしますか？";
+const cancelMsg = "マッチングを解消しますか？";
+const modal = ref(false);
+
+const showModal = () => {
+  // モーダル表示する際の処理が必要ならここに書く
+  modal.value = true;
+  console.log("aaa",modal.value)
+};
+
+const postMatch = async (matchStatus: number) => {
+  // モーダルを非表示にして、モーダルでの選択結果によって処理を変える
+  console.log("aaa")
+  modal.value = false;
+  const sendUserId = 2
+  const receiveUserId = route.query.receiveUserId
+  const userType = 2
+  if (matchStatus != null) {
+    await http.post('/userDetail/regMatch', { matchStatus, sendUserId, receiveUserId, userType })
+  }
+};
 </script>
